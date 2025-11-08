9507ee3724a186f9a59e78ddfcea110f9d293274
brend.kiev.ua <besencevaleksandr4@gmail.com>
2025-11-08 13:02:48 +0300

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,18 @@
+let contactBook = {
+    contacts: [
+      { name: "Олександр", phone: "123456789", email: "olex@example.com" },
+      { name: "Мария", phone: "987654321", email: "maria@example.com" }
+    ]
+  };
+  
+let seach="Олександр"
+let found="Не найдено"
+for(let i=0;i<contactBook.contacts.length;i++){
+    if(contactBook.contacts[i].name===seach){
+        found=contactBook.contacts[i]
+    }
+
+}
+console.log(found)
+contactBook.contacts.push({name:"Иван",phone:"123455667",email:"ivan@example.com"})
+console.log(contactBook.contacts)
